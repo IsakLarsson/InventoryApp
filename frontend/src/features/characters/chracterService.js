822a51f4ad6f9ac7699cddd894de456c65af5e63
Isak Larsson <55209313+IsakLarsson@users.noreply.github.com>
2022-07-21 16:42:47 +0200

    save

--- frontend/src/features/characters/chracterService.js
+++ frontend/src/features/characters/chracterService.js
@@ -3,79 +3,84 @@ import axios from "axios";
 const API_URL = "http://192.168.0.166:3000/api/characters/";
 
 const createCharacter = async (characterData, token) => {
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    };
 
-  const response = await axios.post(API_URL, characterData, config);
+    const response = await axios.post(API_URL, characterData, config);
 
-  return response.data;
+    return response.data;
 };
 
 const getCharacters = async (token) => {
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    };
 
-  const response = await axios.get(API_URL, config);
+    const response = await axios.get(API_URL, config);
 
-  return response.data;
+    return response.data;
 };
 
 const deleteCharacter = async (characterId, token) => {
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
-  const response = await axios.delete(API_URL + characterId, config);
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    };
+    const response = await axios.delete(API_URL + characterId, config);
 
-  return response.data;
+    return response.data;
 };
 
 const addItem = async (id, item, token) => {
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
-  console.log("ADding item: ", item);
-  const response = await axios.post(`${API_URL}${id}/inventory`, item, config);
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    };
+    console.log("ADding item: ", item);
+    const response = await axios.post(
+        `${API_URL}${id}/inventory`,
+        item,
+        config
+    );
 
-  return response.data;
+    return response.data;
 };
 
 const deleteItem = async (id, item, token) => {
-  const itemID = item._id;
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
+    const itemID = item._id;
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    };
 
-  console.log("Deleting item: ", item);
-  console.log("TOKEN SENT", token);
-  console.log("character ID", id);
-  const data = { itemName: item.itemName };
-  //axios delete seems to be working different from the others
-  const response = await axios.delete(
-    `${API_URL}${id}/inventory/${itemID}`,
-    config
-  );
+    console.log("Deleting item: ", item);
+    console.log("TOKEN SENT", token);
+    console.log("character ID", id);
+    const data = { itemName: item.itemName };
+    //axios delete seems to be working different from the others
+    const response = await axios.delete(
+        `${API_URL}${id}/inventory/${itemID}`,
+        config
+    );
 
-  return response.data;
+    console.log("RESPONSE DELETE:", response);
+    return response.data;
 };
 
 const characterService = {
-  createCharacter,
-  getCharacters,
-  deleteCharacter,
-  addItem,
-  deleteItem,
+    createCharacter,
+    getCharacters,
+    deleteCharacter,
+    addItem,
+    deleteItem,
 };
 
 export default characterService;
