78c25fa07959fce9e961f3de1a2cc9d15e760baa
Isak Larsson <55209313+IsakLarsson@users.noreply.github.com>
2022-07-21 16:42:17 +0200

    save

--- backend/src/controllers/characterController.ts
+++ backend/src/controllers/characterController.ts
@@ -7,200 +7,200 @@ import Item from "../models/itemModel";
 import asyncHandler from "express-async-handler";
 
 export const createCharacter = asyncHandler(async (req: any, res: Response) => {
-  const { name } = req.body;
-  const newCharacter = new Character<CharacterInterface>({
-    name: name,
-    inventory: [],
-    coins: { gold: 0, silver: 0, copper: 40 },
-    user: req.user.id,
-  });
-
-  try {
-    const savedCharacter = await newCharacter.save();
-    res.status(201).json({ created_character: savedCharacter });
-    return;
-  } catch (error) {
-    if (error instanceof Error) {
-      throw new Error(error.message);
-    }
-  }
-});
-
-export const getAllCharacters = asyncHandler(
-  async (req: any, res: Response) => {
-    try {
-      const foundCharacters = await Character.find({
+    const { name } = req.body;
+    const newCharacter = new Character<CharacterInterface>({
+        name: name,
+        inventory: [],
+        coins: { gold: 0, silver: 0, copper: 40 },
         user: req.user.id,
-      }).exec();
-      res.status(200).json({ characters: foundCharacters });
-      return;
-    } catch (error) {
-      if (error instanceof Error) {
-        throw new Error(error.message);
-      }
-    }
-  }
-);
-
-export const getCharacterById = asyncHandler(
-  async (req: Request, res: Response) => {
-    const characterId = req.params.id;
-    if (!idIsValid(characterId)) {
-      throw new Error("Ivalid ID");
-    }
-
-    try {
-      const foundCharacter = await Character.findById(characterId).exec();
-      res.status(200).json({ character: foundCharacter });
-      return;
-    } catch (error) {
-      if (error instanceof Error) {
-        throw new Error(error.message);
-      }
-    }
-  }
-);
-
-export const addItemToInventory = asyncHandler(
-  async (req: Request, res: Response) => {
-    const characterId = req.params.id;
-    const { itemName, value, description } = req.body;
-
-    if (!itemName || !value) {
-      throw new Error("Missing required fields");
-    }
-
-    const newItem = new Item<ItemInterface>({
-      itemName: itemName,
-      value: value,
-      description: description,
     });
 
     try {
-      const foundCharacter = await Character.findById(characterId).exec();
-
-      if (foundCharacter != undefined) {
-        foundCharacter.inventory.push(newItem);
-        await foundCharacter.save();
-        res.status(200).json({ addedItem: newItem });
+        const savedCharacter = await newCharacter.save();
+        res.status(201).json({ created_character: savedCharacter });
         return;
-      } else {
-        res.status(404);
-        throw new Error("Couldnt find that character!");
-      }
     } catch (error) {
-      if (error instanceof Error) {
-        throw new Error(error.message);
-      }
+        if (error instanceof Error) {
+            throw new Error(error.message);
+        }
     }
-  }
+});
+
+export const getAllCharacters = asyncHandler(
+    async (req: any, res: Response) => {
+        try {
+            const foundCharacters = await Character.find({
+                user: req.user.id,
+            }).exec();
+            res.status(200).json({ characters: foundCharacters });
+            return;
+        } catch (error) {
+            if (error instanceof Error) {
+                throw new Error(error.message);
+            }
+        }
+    }
+);
+
+export const getCharacterById = asyncHandler(
+    async (req: Request, res: Response) => {
+        const characterId = req.params.id;
+        if (!idIsValid(characterId)) {
+            throw new Error("Ivalid ID");
+        }
+
+        try {
+            const foundCharacter = await Character.findById(characterId).exec();
+            res.status(200).json({ character: foundCharacter });
+            return;
+        } catch (error) {
+            if (error instanceof Error) {
+                throw new Error(error.message);
+            }
+        }
+    }
+);
+
+export const addItemToInventory = asyncHandler(
+    async (req: Request, res: Response) => {
+        const characterId = req.params.id;
+        const { itemName, value, description } = req.body;
+
+        if (!itemName || !value) {
+            throw new Error("Missing required fields");
+        }
+
+        const newItem = new Item<ItemInterface>({
+            itemName: itemName,
+            value: value,
+            description: description,
+        });
+
+        try {
+            const foundCharacter = await Character.findById(characterId).exec();
+
+            if (foundCharacter != undefined) {
+                foundCharacter.inventory.push(newItem);
+                await foundCharacter.save();
+                res.status(200).json({ addedItem: newItem });
+                return;
+            } else {
+                res.status(404);
+                throw new Error("Couldnt find that character!");
+            }
+        } catch (error) {
+            if (error instanceof Error) {
+                throw new Error(error.message);
+            }
+        }
+    }
 );
 
 export const deleteItemFromInventory = asyncHandler(
-  async (req: Request, res: Response) => {
-    const characterId = req.params.id;
-    const itemID = req.params.itemid;
-    console.log(itemID);
-    const { itemName } = req.body;
+    async (req: Request, res: Response) => {
+        const characterId = req.params.id;
+        const itemID = req.params.itemid;
+        console.log("Deleting item:", itemID);
+        const { itemName } = req.body;
 
-    if (!itemName) {
-      res.status(500).json({ errorMessage: "Invalid body" });
-      return;
-    }
-
-    try {
-      const foundCharacter = await Character.findByIdAndUpdate(
-        characterId,
-        {
-          $pull: { inventory: { _id: itemID } },
-        },
-        {
-          new: true,
+        if (!itemName) {
+            res.status(500).json({ errorMessage: "Invalid body" });
+            return;
         }
-      );
-      if (foundCharacter != undefined) {
-        res.status(200).json({ updatedCharacter: foundCharacter });
-      } else {
-        res.status(404);
-        throw new Error("Couldnt find that character!");
-      }
-    } catch (error) {
-      if (error instanceof Error) {
-        throw new Error(error.message);
-      }
+
+        try {
+            const foundCharacter = await Character.findByIdAndUpdate(
+                characterId,
+                {
+                    $pull: { inventory: { _id: itemID } },
+                },
+                {
+                    new: true,
+                }
+            ).exec();
+            if (foundCharacter != undefined) {
+                res.status(200).json({ updatedCharacter: foundCharacter });
+            } else {
+                res.status(404);
+                throw new Error("Couldnt find that character!");
+            }
+        } catch (error) {
+            if (error instanceof Error) {
+                throw new Error(error.message);
+            }
+        }
     }
-  }
 );
 
 export const deleteCharacterById = asyncHandler(
-  async (req: Request, res: Response) => {
-    const characterId = req.params.id;
-    if (!idIsValid(characterId)) {
-      throw new Error("Invalid ID");
+    async (req: Request, res: Response) => {
+        const characterId = req.params.id;
+        if (!idIsValid(characterId)) {
+            throw new Error("Invalid ID");
+        }
+
+        try {
+            const deletedCharacter = await Character.findByIdAndDelete(
+                characterId
+            ).exec();
+
+            if (!deletedCharacter) {
+                res.status(404);
+                throw new Error("Character not found");
+            }
+
+            res.status(200).json({ deletedCharacter: deletedCharacter });
+            return;
+        } catch (error) {
+            if (error instanceof Error) {
+                throw new Error(error.message);
+            }
+        }
     }
-
-    try {
-      const deletedCharacter = await Character.findByIdAndDelete(
-        characterId
-      ).exec();
-
-      if (!deletedCharacter) {
-        res.status(404);
-        throw new Error("Character not found");
-      }
-
-      res.status(200).json({ deletedCharacter: deletedCharacter });
-      return;
-    } catch (error) {
-      if (error instanceof Error) {
-        throw new Error(error.message);
-      }
-    }
-  }
 );
 
 export const changeCoinsById = asyncHandler(
-  async (req: Request, res: Response) => {
-    const characterId = req.params.id;
-    const newCoins: Coins = req.body;
+    async (req: Request, res: Response) => {
+        const characterId = req.params.id;
+        const newCoins: Coins = req.body;
 
-    if (!idIsValid(characterId)) {
-      throw new Error("Invalid ID");
+        if (!idIsValid(characterId)) {
+            throw new Error("Invalid ID");
+        }
+
+        //Fix so that strings are invalid coin values
+
+        try {
+            const foundCharacter = await Character.findById(characterId).exec();
+            if (foundCharacter != undefined) {
+                const currentCoins = foundCharacter.coins;
+                foundCharacter.coins = sumCoins(currentCoins, newCoins);
+                await foundCharacter.save();
+                res.status(200).json({ updatedCharacter: foundCharacter });
+            } else {
+                res.status(404).json({
+                    message: "Couldn't find that character!",
+                });
+            }
+        } catch (error) {
+            if (error instanceof Error) {
+                throw new Error(error.message);
+            }
+        }
     }
-
-    //Fix so that strings are invalid coin values
-
-    try {
-      const foundCharacter = await Character.findById(characterId).exec();
-      if (foundCharacter != undefined) {
-        const currentCoins = foundCharacter.coins;
-        foundCharacter.coins = sumCoins(currentCoins, newCoins);
-        await foundCharacter.save();
-        res.status(200).json({ updatedCharacter: foundCharacter });
-      } else {
-        res.status(404).json({
-          message: "Couldn't find that character!",
-        });
-      }
-    } catch (error) {
-      if (error instanceof Error) {
-        throw new Error(error.message);
-      }
-    }
-  }
 );
 
 const sumCoins = (currentCoins: Coins, newCoins: Coins): Coins => {
-  const totalCoins: Coins = {
-    gold: currentCoins.gold + newCoins.gold,
-    silver: currentCoins.silver + newCoins.silver,
-    copper: currentCoins.copper + newCoins.copper,
-  };
-  return totalCoins;
+    const totalCoins: Coins = {
+        gold: currentCoins.gold + newCoins.gold,
+        silver: currentCoins.silver + newCoins.silver,
+        copper: currentCoins.copper + newCoins.copper,
+    };
+    return totalCoins;
 };
 
 const idIsValid = (id: string) => {
-  if (id.match(/^[0-9a-fA-F]{24}$/)) {
-    return true;
-  }
+    if (id.match(/^[0-9a-fA-F]{24}$/)) {
+        return true;
+    }
 };
--- backend/src/routes/characterRoutes.ts
+++ backend/src/routes/characterRoutes.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import {
-  changeCoinsById,
-  addItemToInventory,
-  createCharacter,
-  deleteCharacterById,
-  deleteItemFromInventory,
-  getAllCharacters,
-  getCharacterById,
+    changeCoinsById,
+    addItemToInventory,
+    createCharacter,
+    deleteCharacterById,
+    deleteItemFromInventory,
+    getAllCharacters,
+    getCharacterById,
 } from "../controllers/characterController";
 import { protect } from "../middleware/authMiddleware";
 
@@ -19,17 +19,17 @@ characterRoutes.get("/api/characters/:id", protect, getCharacterById);
 characterRoutes.get("/api/characters/", protect, getAllCharacters);
 
 characterRoutes.post(
-  "/api/characters/:id/inventory/",
-  protect,
-  addItemToInventory
+    "/api/characters/:id/inventory/",
+    protect,
+    addItemToInventory
 );
 
 characterRoutes.patch("/api/characters/:id/coins", protect, changeCoinsById);
 
 characterRoutes.delete(
-  "/api/characters/:id/inventory/:id",
-  protect,
-  deleteItemFromInventory
+    "/api/characters/:id/inventory/:itemid",
+    protect,
+    deleteItemFromInventory
 );
 
 characterRoutes.delete("/api/characters/:id", protect, deleteCharacterById);
